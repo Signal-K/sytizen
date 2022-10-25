21edd910bbb83e22894223911ffb05d94a8c6a8a
Liam Arbuckle <liam@skinetics.tech>
2022-10-25 15:25:26 +0800

    ⛄️🍀 -> Base styling for sidebar [HDASH-4] & [#6]

--- src/components/Body Section/Body.jsx
+++ src/components/Body Section/Body.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import '../SideBar Section/bodyco.css'; //import './body.css;';
+
+import Top from './Top Section/Top';
+import Listing from './Listing Section/Listing';
+import Activity from './Activity Section/Activity';
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <div className='mainContent'>
+      <Top />
+
+      <div className='bottom flex'>
+        <Listing />
+      </div>
+    </div>
   )
 }
 
