9349039e56974d1957970a75b74c387b51cd496c
Liam Arbuckle <liam@skinetics.tech>
2022-10-31 23:47:34 +0800

    🩸🧀 ↣ Adding more login styling (not quite finished yet)

--- Contracts/Player.sol
+++ Contracts/Player.sol
@@ -34,9 +34,34 @@ contract Mining is ReentrancyGuard, ERC1155Holder {
     function stake(uint256 _tokenId) external nonReentrant {
         require(spaceshipNftCollection.balanceOf(msg.sender, _toeknId) >= 1, "You need at least 1 spaceship of correct type to stake");
 
-        if (playerSpaceship[msg.sender].isData) { // If user already has a spaceship, send it back to them
-            
+        if (playerSpaceship[msg.sender].isData) { // If user already has a spaceship [staked], send it back to them
+            spaceshipNftCollection.safeTransferFrom(address(this), msg.sender, playerSpaceship[msg.sender].value, 1, "Returning your old Spaceship"); // Use safeTransfer to send spaceship back
         }
+
+        uint256 reward = calculateRewards(msg.sender); // Calculate rewards owed to the player
+        rewardsToken.transfe(msg.sender, reward); // Transfer reward tokens based on what's owed
+
+        // Stake component
+        spaceshipNftCollection.safeTransferFrom(msg.sender, address(this), _tokenId, 1, "Staking your spaceship"); // Transfer the spaceship (Still attached to `msg.sender`) to the [staking] contract
+
+        // Update the spaceship mapping
+        playerSpaceship[msg.sender].value = _tokenId;
+        playerSpaceship[msg.sender].isData = true; // Currently being staked
+        playerLastUpdate[msg.sender].isData = true; // Updating `lastUpdate` mapping for player
+        playerLastUpdate[msg.sender].value = block.timestamp;
+    }
+
+    // Function to withdraw your spaceships from the staking vault
+    function withdraw() external nonReentrant {
+        require(playerSpaceship[msg.sender].isData, "You do not have a spaceship you can currently withdraw"); // Ensure user has something to withdraw before proceeding
+        uint256 reward = calculateRewards(msg.sender);
+        rewardsToken.transfer(msg.sender);
+        spaceshipNftCollection.safeTransferFrom(address(this), msg.sender, playerSpaceship[msg.sender].value, 1, "Returned spaceship to msg.sender");
+
+        // Update spaceship mapping
+        playerSpaceship[msg.sender].isData = false;
+        playerLastUpdate[msg.sender].isData = true;
+        playerLastUpdate[msg.sender].value = block.timestamp;
     }
 
     // Calculate rewards the player is owed
@@ -45,6 +70,7 @@ contract Mining is ReentrancyGuard, ERC1155Holder {
             return 0;
         }
 
+        // Start calculating rewards IF `isData` is true
         uint256 timeDifference = block.timestamp - playerLastUpdate[_player].value; // Used to calculate the rewards owed to the player
         uint256 rewards = timeDifference * 10_000_000_000_000 * (playerSpaceship[_player].value + 1); // Equation/algo to calculate rewards owed based on tokenId
 
