17678e43e4f65156dd0f97eba4248caa57df056b
Liam Arbuckle <liam@skinetics.tech>
2022-11-02 00:09:35 +0800

    🥨😅 ↣ Reverting to old auth method

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,44 +1,16 @@
-import React, { useState } from "react";
+
+import React from "react";
 import styled from "styled-components";
 import { Container } from "react-dom";
-import { useHistory } from 'react-router-dom';
 
 // Asset imports
 import bgImg from '../Assets/Pages/bg.png';
 
 // Component imports
 import Sidebar from "../components/auth/home/Sidebar";
-import Main from "../components/auth/home/Main";
-
-// Magic imports
-import { loginUser } from "../service/magic";
+import main from "../components/auth/home/main";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [loading, setLoading] = useState('');
-    const [error, setError] = useState(null);
-    const history = useHistory();
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setLoading(true);
-        if (!email) {
-            setLoading(false);
-            setError('Email is invalid');
-            return;
-        } try {
-            await loginUser(email);
-            setLoading(false);
-            history.replace('/dashboard');
-        } catch (error) {
-            setError('Unable to log in');
-            console.error(error);
-        }
-    };
-
-    const handleChange = (event) => {
-        setEmail(event.target.value);
-    };
-
     const Container = styled.div`
         background: #eefcff;
         position: absolute;
@@ -55,14 +27,12 @@ const Login = () => {
         background-repeat: no-repeat;
         width: 100%;
         height: 100%;
-        display: flex;
     `
 
     return (
         <Container>
             <Wrapper>
-                <Sidebar />
-                <Main />
+                <h1>Test</h1>
             </Wrapper>
         </Container>
     );
