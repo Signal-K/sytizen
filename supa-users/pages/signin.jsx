ec31afb614a3213e42c7ad85c46bc2bceb021a55
Liam Arbuckle <liam@skinetics.tech>
2022-11-06 12:45:22 +0800

    🦁🦄 ↣ Magic now working with Moralis API Key
    Nexstep : get Magic talking to Supabase, Moralis!! Seems to be building fine...

--- supa-users/pages/signin.jsx
+++ supa-users/pages/signin.jsx
@@ -1,46 +1,56 @@
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
-import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/router';
-import { useConnect, useAccount, useSignMessage, useDisconnect } from 'wagmi';
-import axios from 'axios';
+import { MagicAuthConnector } from '@everipedia/wagmi-magic-connector'
+import { signIn } from 'next-auth/react'
+import { useAccount, useConnect, useSignMessage, useDisconnect } from 'wagmi'
+import { useRouter } from 'next/router'
+import axios from 'axios'
 
 function SignIn() {
-    const { connectAsync } = useConnect();
-    const { disconnectAsync } = useDisconnect();
-    const { isConnected } = useAccount();
-    const { signMessageAsync } = useSignMessage();
-    const { push } = useRouter();
+  const { connectAsync } = useConnect({
+    connector: new MagicAuthConnector({
+      options: {
+        apiKey: 'pk_live_C337BC194C080B7D',
+      },
+    }),
+  })
+  const { disconnectAsync } = useDisconnect()
+  const { isConnected } = useAccount()
+  const { signMessageAsync } = useSignMessage()
+  const { push } = useRouter()
 
-    const handleAuth = async () => { // User attempts to authenticate themselves
-        if (isConnected) { // Disconnect web3 provider if already active
-            await disconnectAsync();
-        }
+  const handleAuth = async () => {
+    if (isConnected) {
+      await disconnectAsync()
+    }
 
-        const { account, chain } = await connectAsync({ connector: new MetaMaskConnector() }); // Enable web3 provider for metamask
-        const userData = { address: account, chain: chain.id, network: 'evm' };
-        const { data } = await axios.post('/api/auth/request-message', userData, { // Make post request to 'request-message' endpoint
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
+    const { account } = await connectAsync()
+    const userData = { address: account, chain: '0x1', network: 'evm' }
 
-        const message = data.message;
-        const signature = await signMessageAsync({ message });
+    const { data } = await axios.post('/api/auth/request-message', userData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
 
-        console.log(userData);
-        console.log(signature);
+    const message = data.message
 
-        // Redirect user after auth to /user
-        const { url } = await signIn('credentials', { message, signature, redirect: false, callbackUrl: '/user' });
-        push(url);
-    };
+    const signature = await signMessageAsync({ message })
 
-    return (
-        <div>
-            <h3>Moralis Auth</h3>
-            <button onClick={() => handleAuth()}>Authenticate via Metamask</button>
-        </div>
-    );
+    // redirect user after auth to '/user'
+    const { url } = await signIn('credentials', {
+      message,
+      signature,
+      redirect: false,
+      callbackUrl: '/user',
+    })
+    push(url)
+  }
+
+  return (
+    <div>
+      <h3>Web3 Authentication</h3>
+      <button onClick={() => handleAuth()}>Authenticate via Magic.Link</button>
+    </div>
+  )
 }
 
 export default SignIn;
