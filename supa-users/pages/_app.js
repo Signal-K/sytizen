70947d1de9ba6f5d557e2ca129d4fd2d7480ea37
Liam Arbuckle <liam@skinetics.tech>
2022-11-08 01:39:21 +0800

    🍎🧚🏾‍♂️ ↣ Stuck on supa -> reverting

--- supa-users/pages/_app.js
+++ supa-users/pages/_app.js
@@ -1,7 +1,17 @@
+// Styling imports
 import '../styles/globals.css';
+
+// Moralis + Supa | Moralis + Magic components
 import { createClient, configureChains, defaultChains, WagmiConfig } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
 import { SessionProvider } from 'next-auth/react';
+
+// Supabase + Magic components
+import { useRouter } from 'next/router';
+import { supabase } from './api/client';
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import { checkResultErrors } from 'ethers/lib/utils';
 
 const { provider, webSocketProvider } = configureChains(defaultChains, [publicProvider()]);
 
@@ -12,13 +22,36 @@ const client = createClient({
 });
 
 function MyApp({ Component, pageProps }) {
+  const [authenticatedState, setAuthenticatedState] = useState('not-authenticated'); // auth state, retrieved from supa. defaults to not logged in
+  const router = useRouter();
+
+  useEffect(() => { // Set up supabase on/off auth state listener
+    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+      handleAuthChange(event, session);
+      if (event === 'SIGNED_IN') { // This event only fires if the auth event occurs (e.g. being rerouted from the magic link that lands in their inbox)
+        setAuthenticatedState('authenticated');
+        router.push('/profile'); // Reroute user to profile if they're signed in
+      }
+      if (event === 'SIGNED_OUT') {
+        setAuthenticatedState('not-authenticated');
+      }
+    })
+    checkResultErrors();
+    return () => {
+      authListener.unsubscribe();
+    }
+  }, []);
+
   return (
     <WagmiConfig client={ client }>
       <SessionProvider session = {pageProps.session} refetchInterval={0}>
-        <Component {...pageProps} />
+        <div>
+          <nav></nav>
+          <Component {...pageProps} />
+        </div>
       </SessionProvider>
     </WagmiConfig>
   );
 }
 
-export default MyApp
+export default MyApp;
--- supa-users/pages/index.js
+++ supa-users/pages/index.js
@@ -1,9 +1,17 @@
 //import { useEffect, useState } from 'react';
 //import { supabase } from '../lib/initSupabase';
 import SignIn from './signin';
+import styles from '../styles/home.module.css';
 
 export default function Home() {
   return(
-    <SignIn />
+    <div className={styles.container}>
+      {/*<SignIn />*/}
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Hello World
+        </h1>
+      </main>
+    </div>
   )
 }
