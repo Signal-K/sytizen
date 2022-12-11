2f9c8d24f568f8b366c8c4f9ecd89f70359c8b47
Liam Arbuckle <liam@skinetics.tech>
2022-12-11 11:58:35 +0000

    🦔🦉 ↣ Adding a new component (supabase-style reddit) to our backend primer

--- supa-users/server/models.py
+++
@@ -1,6 +0,0 @@
-from . import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True) # This should match with Supabase?
-    email = db.Column(db.String(50))
-    magicId = db.Column(db.String(100))
