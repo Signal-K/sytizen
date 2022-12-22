e5656dd071642184d01d82617b362f323b5df286
Liam Arbuckle <liam@skinetics.tech>
2022-12-22 18:41:56 +1100

    🧖🏻‍♂️🥼 ↣ Beginning migration from Vite to Next for proposal section [#16]. A note that there is a bug with voting on Proposals Board -> tasked on Linear

---
+++ client/client/pages/_app.jsx
@@ -0,0 +1,10 @@
+import '../styles/globals.css';
+
+// Imports for proposals/voting section
+import { ProposalDetails, CreateProposal, Home, Profile } from './proposals';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp;
