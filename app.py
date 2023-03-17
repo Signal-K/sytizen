d3bdb1b0179ef8ee2b9e9931ad6f2b3c6807af8f
Liam Arbuckle <liam@skinetics.tech>
2023-03-17 15:34:32 +1100

    🧬🪐 ↝ Planet generator now running when requested by #16, returns image as request for #18

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, jsonify, Blueprint
+from flask import Flask, request, make_response, jsonify, Blueprint, send_file
 from flask_cors import CORS, cross_origin
 from supabase_py import client, create_client
  
@@ -8,7 +8,9 @@ from io import BytesIO
 from .datastore import supabase, find_all_planets, add_planet_to_DB
 # from Generator import gen_image # add this to a blueprint
 
-from views import main
+from .views import main
+
+from .main import gen_image
 
 # Flask application/container initialisation
 app = Flask(__name__)
@@ -18,9 +20,17 @@ app.register_blueprint(main, url_prefix='/classify')
 CORS(app)
 
 if __name__ == '__main__':
+    for seed in range(0, 10):
+        gen_image(seed)
     app.run(debug = True)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    for seed in range(0, 10):
+        gen_image(seed)
     return 'hello world'
+
+@app.route('/get_image')
+def get_image():
+    return send_file('out0.png', mimetype='image/png')
