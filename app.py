110e248069dec2f4067a6dc1e006baec42e4d5f3
Liam Arbuckle <liam@skinetics.tech>
2023-09-12 12:35:08 +1000

    🪰🍚 ↝ Login route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, Blueprint, jsonify
 import lightkurve as lk
 import os
-from supabase import create_client
+# from supabase import create_client
 
 """# Flask blueprints ""
 from database.classify import classify
@@ -12,6 +12,29 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello world"
+
+@app.route('/login', methods=['POST'])
+def login():
+    # Get the username and password from the request data
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # Check if username and password are provided
+    if not username or not password:
+        return jsonify({'error': 'Username and password are required'}), 400
+
+    # Send a POST request to the NASA Earthdata login API
+    response = requests.post(
+        'https://appeears.earthdatacloud.nasa.gov/api/login',
+        auth=(username, password)
+    )
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        token_response = response.json()
+        return jsonify(token_response)
+    else:
+        return jsonify({'error': 'Login failed'}), 401
 
 if __name__ == "__main__":
     app.run()
--- data/appeears/randomProduct.py
+++ data/appeears/randomProduct.py
@@ -6,4 +6,54 @@ import random
 response = requests.get(
     'https://appeears.earthdatacloud.nasa.gov/api/product')
 product_response = response.json()
-print(product_response)
+# print(product_response)
+
+json.dumps(product_response[0])
+loc = input('location: ')
+geoloc = requests.get('https://geocode.maps.co/search?q={' + loc + '}')
+print(geoloc.json())
+
+username = 'mcdepth'
+pswd = 'VgZ#MNYz5&Fv$@TPF!q*kJCeKw' # getpass.getpass('Password: ')
+
+response = requests.post('https://appeears.earthdatacloud.nasa.gov/api/login', auth=(username, pswd))
+token_response = response.json()
+print(token_response)
+
+token = token_response['token']
+response = requests.get(
+    'https://appeears.earthdatacloud.nasa.gov/api/task', 
+    headers={'Authorization': 'Bearer {0}'.format(token)})
+task_response = response.json()
+print(task_response)
+
+product_id = 'SRTMGL1_NUMNC.003'
+response = requests.get('https://appeears.earthdatacloud.nasa.gov/api/product/{0}'.format(product_id))
+layer_response = response.json()
+print(layer_response)
+
+ranProd = random.choice(product_response)
+ranProd
+
+response = requests.get('https://appeears.earthdatacloud.nasa.gov/api/product/{0}'.format(ranProd['ProductAndVersion']))
+layer_response = response.json()
+print(layer_response)
+
+response = requests.get('https://appeears.earthdatacloud.nasa.gov/api/spatial/proj')
+proj_response = response.json()
+print(proj_response)
+
+response = requests.get('https://appeears.earthdatacloud.nasa.gov/api/quality/{0}'.format(ranProd['ProductAndVersion']))
+quality_response = response.json()
+print(quality_response)
+
+quality_response[0]['QualityLayers'][0]
+
+response = requests.get('https://appeears.earthdatacloud.nasa.gov/api/quality/{0}/{1}'.format(quality_response[0]['ProductAndVersion'], quality_response[0]['QualityLayers'][0]))
+quality_response = response.json()
+print(quality_response)
+
+quality_value = 0
+response = requests.get('https://appeears.earthdatacloud.nasa.gov/api/quality/{0}/{1}/{2}'.format(quality_response[0]['ProductAndVersion'], quality_response[0]['QualityLayer'][0], quality_value))
+quality_response = response.json()
+print(quality_response)
