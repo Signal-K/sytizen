26ca2f7dcec1c4277b6f6362bf8c1bba173788e3
Gizmotronn <liam@skinetics.tech>
2024-10-13 15:01:11 +0800

    🪁🐦 ↝ [SSP-25 SSP-26 SSP-27 SSP-28 SSM-15]: Data collection for three new projects

--- buckets/upload.py
+++ buckets/upload.py
@@ -36,10 +36,10 @@ def insert_into_anomalies(supabase: Client, anomaly_id, content, anomaly_set: st
                 "id": anomaly_id, 
                 "content": content, 
                 # "anomalytype": 'planet',
-                "anomalytype": "telescopeClouds",
+                "anomalytype": "satellitePics",
                 # "anomalySet": anomaly_set,
-                "anomalySet": "lidar-EarthClouds",
-                "parentAnomaly": 69, #69
+                "anomalySet": "satellite-planetFout",
+                "parentAnomaly": 40, #69
             }
             response = supabase.table('anomalies').insert(data).execute()
             print(f"Inserted anomaly with id {anomaly_id} into 'anomalies' table.")
@@ -74,7 +74,7 @@ def upload_directory_to_supabase(supabase: Client, bucket_name: str, local_direc
 def main():
     supabase = init_supabase_client()
     bucket_name = "telescope"
-    local_directory = "telescope" 
+    local_directory = "satellite/satellite-planetFour" 
     
     upload_directory_to_supabase(supabase, bucket_name, local_directory)
 
